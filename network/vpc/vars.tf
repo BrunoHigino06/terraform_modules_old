f1eeba4c48980cbc83d850fd91668edd69db794f
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-26 18:39:20 -0300

    2022.03.26

--- network/vpc/vars.tf
+++ network/vpc/vars.tf
@@ -1,8 +1,12 @@
-variable "vpc" {
+variable "docker_ports" {
+  type = list(object({
+    cidr_block = string
+    Name       = string
+  }))
   default = [
     {
       cidr_block = ""
-      Name = ""
+      Name       = ""
     }
   ]
 }
