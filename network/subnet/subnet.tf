6fbdd91b193d9add0d85aa6eaf73ba312e998601
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-27 19:48:41 -0300

    2022.03.27

--- network/subnet/subnet.tf
+++ network/subnet/subnet.tf
@@ -1,17 +1,17 @@
 resource "aws_subnet" "public" {
-    count = var.public_subnet_count
+    count = length(var.subnet.public.cidr_block)
     vpc_id     = var.vpc_id
     cidr_block = var.subnet.public.cidr_block
     tags = {
-        Name = var.subnet.public.Name
+        Name = "${var.subnet.public.Name}_${count.index}"
     }
 }
 
 resource "aws_subnet" "private" {
-    count = var.private_subnet_count
+    count = length(var.subnet.private.cidr_block)
     vpc_id     = var.vpc_id
     cidr_block = var.subnet.private.cidr_block
     tags = {
-        Name = var.subnet.private.Name
+        Name = "${var.subnet.private.Name}_${count.index}"
     }
 }
