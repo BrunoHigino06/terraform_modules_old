d13779e3c118f4c2dda58be12e4ac320e9d2724d
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-04-24 12:10:19 -0300

    Update vars.tf

--- network/subnet/vars.tf
+++ network/subnet/vars.tf
@@ -3,11 +3,11 @@ variable "vpc_id" {}
 variable "name_subnets" {
   description = "Name of the subnets"
   type = list(string)
-  default = ["subnet1", "subnet2"]
+  default = [""]
 }
 
 variable "cidr_block_subnets" {
     type = list(string)
     description = "List of cidr blocks of public subnets"
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    default = [""]
 }
