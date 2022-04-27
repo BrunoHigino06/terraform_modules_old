dabc24bb32f2f01dc2b019c0dd34f2e65618a007
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-04-27 04:00:18 -0300

    2022.04.27

---
+++ AWS/network/routetableassociation/routetableassociation.tf
@@ -0,0 +1,36 @@
+data "aws_subnet" "filter_subnet_id" {
+    count = length(var.subnet_name_association)
+    filter {
+        name = "tag:Name"
+        values = var.subnet_name_association[count.index]
+    }
+}
+
+locals {
+    description = "id's to associate with route table"
+    subnet_ids = aws_subnet.filter_subnet_id[*].id
+}
+
+data "aws_route_table" "filter_route_table_id" {
+    count = length(var.route_table_id)
+        filter {
+            name = "tag:Name"
+            values = var.route_table_id[count.index]
+        }
+}
+
+locals {
+    description = "id's to associate with subnets"
+    route_table_id = aws_route_table.filter_route_table_id[*].Id
+}
+
+resource "aws_route_table_association" "aws_route_table_association" {
+    count = length(var.subnet_name_association)
+    subnet_id = local.subnet_ids[count.index]
+    route_table_id = local.route_table_id[count.index]
+
+    depends_on = [
+      data.aws_route_table.filter_route_table_id,
+      data.aws_subnet.filter_subnet_id
+    ]
+}
---
+++ AWS/network/routetableassociation/vars.tf
@@ -0,0 +1,9 @@
+variable "subnet_name_association" {
+  type = list(string)
+  description = "Names of the subnets to associate with a route table"
+}
+
+variable "route_table_id" {
+  type = list(string)
+  description = "Id's of the route tables to association with the subnets"
+}
